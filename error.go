aeb63552c2a3cc8e1256971aea6a2c819313aef0
sandy <18382255942@163.com>
2021-09-02 14:37:26 +0800

    feat(): fix client err

--- error.go
+++ error.go
@@ -5,6 +5,6 @@ import "errors"
 var (
 	ErrNotFound   = errors.New("Not Found")
 	ErrPendingTx  = errors.New("Pending")
-	ErrInvalidId  = errors.New("Invalid ar tx id")
-	ErrBadGateway = errors.New("Bad gateway")
+	ErrInvalidId  = errors.New("Invalid ArId")
+	ErrBadGateway = errors.New("Bad Gateway")
 )
