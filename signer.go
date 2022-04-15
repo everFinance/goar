3dff977fd4298d868a7c4b0bad33095ad57e9327
sandy <18382255942@163.com>
2022-04-15 16:00:57 +0800

    fix(): add signer

---
+++ signer.go
@@ -0,0 +1,81 @@
+package goar
+
+import (
+	"crypto/rsa"
+	"crypto/sha256"
+	"fmt"
+	"github.com/everFinance/goar/types"
+	"github.com/everFinance/goar/utils"
+	"github.com/everFinance/gojwk"
+	"io/ioutil"
+)
+
+type Signer struct {
+	Address string
+	PubKey  *rsa.PublicKey
+	PrvKey  *rsa.PrivateKey
+}
+
+func NewSignerFromPath(path string) (*Signer, error) {
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	return NewSigner(b)
+}
+
+func NewSigner(b []byte) (*Signer, error) {
+	key, err := gojwk.Unmarshal(b)
+	if err != nil {
+		return nil, err
+	}
+	pubKey, err := key.DecodePublicKey()
+	if err != nil {
+		return nil, err
+	}
+	pub, ok := pubKey.(*rsa.PublicKey)
+	if !ok {
+		err = fmt.Errorf("pubKey type error")
+		return nil, err
+	}
+
+	prvKey, err := key.DecodePrivateKey()
+	if err != nil {
+		return nil, err
+	}
+	prv, ok := prvKey.(*rsa.PrivateKey)
+	if !ok {
+		err = fmt.Errorf("prvKey type error")
+		return nil, err
+	}
+	addr := sha256.Sum256(pub.N.Bytes())
+	return &Signer{
+		Address: utils.Base64Encode(addr[:]),
+		PubKey:  pub,
+		PrvKey:  prv,
+	}, nil
+}
+
+func (s *Signer) SignTx(tx *types.Transaction) error {
+	signData, err := utils.GetSignatureData(tx)
+	if err != nil {
+		return err
+	}
+	sig, err := utils.Sign(signData, s.PrvKey)
+	if err != nil {
+		return err
+	}
+
+	txId := sha256.Sum256(sig)
+	tx.ID = utils.Base64Encode(txId[:])
+	tx.Signature = utils.Base64Encode(sig)
+	return nil
+}
+
+func (s *Signer) Owner() string {
+	return utils.Base64Encode(s.PubKey.N.Bytes())
+}
+
+func (s *Signer) SignMsg(msg []byte) ([]byte, error) {
+	return utils.Sign(msg, s.PrvKey)
+}
