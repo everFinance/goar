59c4902bea94da514494a1e01753d4b5273afa02
sandy <18382255942@163.com>
2022-06-20 14:40:00 +0800

    feat(): bundle test

--- example/bundle_test.go
+++ example/bundle_test.go
@@ -16,40 +16,25 @@ func TestBundle_SendBundleTx(t *testing.T) {
 	itemSdk, err := goar.NewItemSdk(signer, arseedUrl)
 	assert.NoError(t, err)
 
-	target := "Fkj5J8CDLC9Jif4CzgtbiXJBnwXLSrp5AaIllleH_yY"
 	tags := []types.Tag{
-		{Name: "goar", Value: "bundleTx"},
+		{Name: "Content-Type", Value: "image/jpeg"},
 		{Name: "App-Version", Value: "2.0.0"},
 	}
 
-	item01, err := itemSdk.CreateAndSignItem([]byte("goar bundle tx 01"), target, "", tags)
-	assert.NoError(t, err)
-	item02, err := itemSdk.CreateAndSignItem([]byte("goar bundle tx 02"), target, "", tags)
-	assert.NoError(t, err)
-	item03, err := itemSdk.CreateAndSignItem([]byte("goar bundle tx 03"), target, "", tags)
-	assert.NoError(t, err)
-	item04, err := itemSdk.CreateAndSignItem([]byte("goar bundle tx 04"), target, "", tags)
-	assert.NoError(t, err)
-	item05, err := itemSdk.CreateAndSignItem([]byte("goar bundle tx 05"), target, "", tags)
-	assert.NoError(t, err)
-	item06, err := itemSdk.CreateAndSignItem([]byte("goar bundle tx 06"), target, "", tags)
-	assert.NoError(t, err)
-	item07, err := itemSdk.CreateAndSignItem([]byte("goar bundle tx 07"), target, "", tags)
-	assert.NoError(t, err)
-	item08, err := itemSdk.CreateAndSignItem([]byte("goar bundle tx 08"), target, "", tags)
-	assert.NoError(t, err)
-	item09, err := itemSdk.CreateAndSignItem([]byte("goar bundle tx 09"), target, "", tags)
-	assert.NoError(t, err)
-	item10, err := itemSdk.CreateAndSignItem([]byte("goar bundle tx 10"), target, "", tags)
+	data := []byte("123456")
+	item01, err := itemSdk.CreateAndSignItem(data, "", "", tags)
 	assert.NoError(t, err)
 
-	items := []types.BundleItem{item01, item02, item03, item04, item05, item06, item07, item08, item09, item10}
+	items := []types.BundleItem{item01}
 
 	// send item to arseed gateway
 	for _, item := range items {
-		resp, err := itemSdk.SubmitItem(item, "USDC")
+		// t.Log(item.Id)
+		err = utils.VerifyBundleItem(item)
 		assert.NoError(t, err)
-		t.Log(resp)
+		resp, err := itemSdk.SubmitItem(item, "USDT")
+		assert.NoError(t, err)
+		t.Log(*resp)
 	}
 }
 
